81a88cc0442b14523f6ffedfc4b3a68ca741141a
April Yu <yu.april.s@gmail.com>
2014-02-24 00:14:00 -0800

    implemented sentence translation

--- Translation.java
+++ Translation.java
@@ -1,11 +1,30 @@
 import java.util.List;
 
 public class TranslateFrenchToEnglish {
-	public static void translate() {
-		
+
+	public String getTranslation(String foreignWord) {
+
+	}
+
+	public List<String> translateSentence(List<String> sentence) {
+		if (sentence.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<String> bestSentence = new ArrayList<String>();
+        for(int i = 0; i < sentence.size(); i++) {
+        	String frenchWord = sentence[i];
+        	String englishTranslation = TranslateFrenchToEnglish.getTranslation(frenchWord);
+        	bestSentence.append(englishTranslation);
+        }
+        return bestSentence;
+	}
+
+	public static void eval() {
+
 	}
 
 	public static void main(String[] args) {
-        TranslateFrenchToEnglish.translate();
+        TranslateFrenchToEnglish.eval();
     }
 }
