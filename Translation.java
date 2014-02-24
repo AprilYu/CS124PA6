989420d3a94839bf0a387303a96f3d56f8cf8e0f
April Yu <yu.april.s@gmail.com>
2014-02-24 00:01:47 -0800

    created files

---
+++ Translation.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class TranslateFrenchToEnglish {
+	public static void translate() {
+		
+	}
+
+	public static void main(String[] args) {
+        TranslateFrenchToEnglish.translate();
+    }
+}
