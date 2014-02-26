06af175bc72ddcf821abf4c27e9f7e01ed5c3e13
Rush Moody <rush8192@gmail.com>
2014-02-25 22:20:48 -0800

    working baseline translation system with correct dictionary and test/dev sentence split. currently just translates 1-to-1 with no post-processing

--- Translation.java
+++ Translation.java
@@ -6,8 +6,8 @@ public class Translation {
 	static ArrayList<ArrayList<String>> sentences;
 	static Map<String, ArrayList<String>> dictionary;
 	
-	public static final String DEFAULT_DICT = ".csv";
-	public static final String DEV_SENTENCES = ".txt";
+	public static final String DEFAULT_DICT = "data/dictionary.csv";
+	public static final String DEV_SENTENCES = "data/trainSentences.txt";
 
 	public Translation(String corpusFileName, String dictionaryFileName) {
 		try {
@@ -32,12 +32,14 @@ public class Translation {
 		while((line = rd.readLine()) != null) {
 			line = line.toLowerCase();
 			String[] splitLine = line.split(",");
-			String word = splitLine[0];
+			String word = splitLine[0].trim();
 			if (splitLine.length < 2)
 				throw new IOException("Error: no provided translations for word " + word + " on line " + lineNum);
 			ArrayList<String> possibleTranslations = new ArrayList<String>();
 			for (int i = 1; i < splitLine.length; i++) {
-				possibleTranslations.add(splitLine[i]);
+				String trans = splitLine[i].trim();
+				if (!"".equals(trans))
+					possibleTranslations.add(trans);
 			}
 			dict.put(word, possibleTranslations);
 			lineNum++;
@@ -71,20 +73,69 @@ public class Translation {
 	 */
 	private ArrayList<String> tokenizeLine(String line) {
 		ArrayList<String> tokenizedLine = new ArrayList<String>();
+		String[] splitLine = line.split(" ");
+		for (String token : splitLine) {
+			List<String> cleanedToken = removePunctuation(token);
+			for (String token2 : cleanedToken) {
+				tokenizedLine.add(token2.trim());
+			}
+		}
+		return tokenizedLine;
+	}
+	
+	private List<String> removePunctuation(String token) {
+		List<String> cleanedToken = new ArrayList<String>();
 		StringBuilder builder = new StringBuilder();
-		for (int i = 0; i < line.length(); i++) {
-			char ch = line.charAt(i);
-			if (!Character.isLetter(ch)) {
+		for (int i = 0; i < token.length(); i++) {
+			String toCheck = token.substring(i, i+1);
+			if (isPunctuation(toCheck)) {
 				if (builder.length() > 0) {
-					tokenizedLine.add(builder.toString());
+					cleanedToken.add(builder.toString());
 				}
 				builder = new StringBuilder();
 			} else {
-				builder.append(ch);
+				builder.append(toCheck);
 			}
 		}
-		System.out.println("Tokenized "  + line + " into " + tokenizedLine);
-		return tokenizedLine;
+		if (builder.length() > 0) {
+			cleanedToken.add(builder.toString());
+		}
+		return cleanedToken;
+	}
+	
+	private boolean isPunctuation(String token) {
+		if (token.length() == 0)
+			return true;
+		switch (token.charAt(0)) {
+			case '.':
+			case ',':
+			case '!':
+			case '\'':
+			case '"':
+			case '+':
+			//case '-':
+			//case "_":
+			case '&':
+			case '$':
+			case '#':
+			case '@':
+			case '~':
+			case '`':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			case '[':
+			case ']':
+			case '?':
+			case ';':
+			case ':':
+			case '^':
+			case '*':
+				return true;
+			default:
+				return false;
+		}
 	}
 
 	public static String convertListToString(ArrayList<String> sentence) {
@@ -100,11 +151,16 @@ public class Translation {
 		//FOR LATER: implement n-gram stuff to choose best translation of the word.
 		//May need to change function header for this ^^ depending on choice of n
 		ArrayList<String> possibleTranslations = dictionary.get(foreignWord);
+		if (possibleTranslations == null) {
+			System.out.println("Error: No entry for word " + foreignWord);
+			return "NO_TRANS";
+		}
 		return possibleTranslations.get(0);
 	}
     
     public static ArrayList<String> trimPastTense(ArrayList<String> sentence){
         //look at part of speech tags and remove auxilary verbs from passe compose constructions (either look at word before word with past participle tag and remove if etre or avoir, or find tag for these auxilary verbs)
+		return null;
     }
 
 	public static ArrayList<String> translateSentence(ArrayList<String> sentence) {
